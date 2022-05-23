0e8bb1e836e0241c32ad27c56230576e2f94c2b1
root <root@ip-172-31-77-55.ec2.internal>
2022-05-23 08:47:50 +0000

    Fix Replace Random Generator
    In file: fields.py, there is a method  that uses the pseudo-random number generator from {random} module. But the Python documentation suggests that the pseudo-random number generators of this module should not be used for security purposes. Instead, random number generators from the {secrets} module should be used. iCR replaced the usage of random number generators from the {random} module with the random number generator from the {secrets} module.

--- tests/custom_pk/fields.py
+++ tests/custom_pk/fields.py
@@ -1,4 +1,4 @@
-import random
+import secrets
 import string
 
 from django.db import models
@@ -29,7 +29,7 @@ class MyWrapperField(models.CharField):
     def pre_save(self, instance, add):
         value = getattr(instance, self.attname, None)
         if not value:
-            value = MyWrapper(''.join(random.sample(string.ascii_lowercase, 10)))
+            value = MyWrapper(''.join(secrets.SystemRandom().sample(string.ascii_lowercase, 10)))
             setattr(instance, self.attname, value)
         return value
 
